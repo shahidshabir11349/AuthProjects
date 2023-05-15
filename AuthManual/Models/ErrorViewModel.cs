bb536cee6d7dbbd631c745153ce623fd23b0871d
PureJoyMind <mazdak.parniandg@gmail.com>
2023-05-15 19:29:37 +0330

    Add project files.

---
+++ AuthManual/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthManual.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
