81321865ce727405ee9c5f8997ee2a0d39e8f473
PureJoyMind <mazdak.parniandg@gmail.com>
2023-06-09 00:10:37 +0330

    Created claims store and view model

---
+++ AuthManual/Data/ClaimStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace AuthManual.Data
+{
+    public class ClaimStore
+    {
+        public static List<Claim> ClaimsList = new List<Claim>()
+        {
+            new Claim("Create", "Create"),
+            new Claim("Edit", "Edit"),
+            new Claim("Delete", "Delete"),
+        };
+    }
+}
---
+++ AuthManual/Models/UserClaimsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace AuthManual.Models
+{
+    public class UserClaimsViewModel
+    {
+        public UserClaimsViewModel()
+        {
+            UserClaims = new List<UserClaim>();
+        }
+
+        public string UserId { get; set; }
+        public List<UserClaim> UserClaims { get; set; }
+    }
+
+    public class UserClaim // for individual user claim
+    {
+        public string? ClaimType { get; set; }
+        public bool IsSelected  { get; set; }
+    }
+}
